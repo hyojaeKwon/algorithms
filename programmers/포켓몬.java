783c771e2eb673181f72f67a5d9e9118131028dd
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2025-02-15 23:27:25 +0900

    포켓몬

---
+++ programmers/포켓몬.java
@@ -0,0 +1,16 @@
+package programmers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class 포켓몬 {
+    class Solution {
+        public int solution(int[] nums) {
+            Set<Integer> map = new HashSet<>();
+            for (int target : nums) {
+                map.add(target);
+            }
+            return Math.min(nums.length / 2, map.size());
+        }
+    }
+}
